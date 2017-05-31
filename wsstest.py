e1f61738db71a6542834da7cf85d6f95f01d9f0f
Nicholas Falco <nick@iris.washington.edu>
2017-05-30 22:20:46 -0700

    Code cleanup. Improved comments and command line help text.

--- wsstest.py
+++ wsstest.py
@@ -43,15 +43,16 @@ def parse_arguments():
     parser = ArgumentParser(description='Command line parser for a test Web Service Shell handler.')
     parser.add_argument('--format', help="Type of data requested. Choose from 'text', 'xml', and 'zip'", default='text', type=str)
     parser.add_argument('--num_values', help="The number of random values that the program should return.", default=50, type=int)
-    parser.add_argument('--force_error', help=("Force the handler to exit with an error before or after writing to output. "
+    parser.add_argument('--force_error', help=("Optionally close the handler with an error at a certain state of the program execution."
                                              "Choose between 'before', 'runtime', 'after' indicating whether to exit "
-                                             "with an error before, at runtime (while), or after writing to std-out."), type=str, default="")
-    parser.add_argument('--error_msg', help="Force a particular error message to be written to stderror.", type=str)
-    parser.add_argument('--force_exit_code', help="Force the handler to exit with a particular exit code when an error occurs.", type=int)
-    parser.add_argument('--force_sleep', help="Force the handler to sleep for a certain number of seconds before exiting.", default=0, type=float)
-    parser.add_argument('--runtime_error_level', help="Set the minimum level of recursion (deepness) to call before raising a runtime error.", default=0, type=int)
+                                             "with an error before, at runtime, or after writing to std-out."), type=str, default="")
+    parser.add_argument('--error_msg', help="Optionally specify a custom error message to exit the handler with. (write to std-error)", type=str)
+    parser.add_argument('--force_exit_code', help="Optionally force the exit status code to exit the handler with.", type=int)
+    parser.add_argument('--force_sleep', help="Optionally force the handler to sleep for a certain number of seconds before exiting.", default=0, type=float)
+    parser.add_argument('--runtime_error_level', help="Optionally specify the level of the stack trace when forcing a runtime error.", default=0, type=int)
     args = parser.parse_args()
     return args
+
 
 class WSSTest(object):
     
@@ -114,10 +115,12 @@ class WSSTest(object):
            @param num integer: the number of random values 
            to produce
         '''
+        text_output = ", ".join([str(random.randint(0, 100)) for x in xrange(num)])
+
         if self.args.force_error.lower() == "runtime":
             self.raise_runtime_error()
-        return ", ".join([str(random.randint(0, 100)) for x in xrange(num)])
-    
+
+        return text_output
     
     def get_xml(self, num):
         '''Return a XML structure of random numbers between 0 and 99.
@@ -129,14 +132,15 @@ class WSSTest(object):
         for i in xrange(num):
             rand_val = str(random.randint(0, 100))
             ET.SubElement(values, "field_{0}".format(i), name="value_{0}".format(rand_val)).text = rand_val
+
         if self.args.force_error.lower() == "runtime":
             self.raise_runtime_error()
+
         tree = ET.ElementTree(root)
         st = StringIO()
         tree.write(st)
         return st.getvalue() # return the XML file as a string
-    
-    
+
     def get_zip(self, num):
         '''Return a zip file archive containing files with
            up to 10 comma separated random numbers in each file 
@@ -156,8 +160,10 @@ class WSSTest(object):
                     z.writestr('file_{0}'.format(idx), self.get_text(num))
                     num = 0
                 idx = idx + 1
+
             if self.args.force_error.lower() == "runtime":
                 self.raise_runtime_error()
+
         return stio.getvalue()
     
     def process(self):
@@ -169,11 +175,12 @@ class WSSTest(object):
         elif args.format.lower() == "zip":
             sys.stdout.write(wsstest.get_zip(num_values))
 
+
 if __name__ == '__main__':
     args = parse_arguments()
     wsstest = WSSTest(args)
 
-    DEFAULT_EXIT_CODE = 1
+    DEFAULT_EXIT_CODE = 1 # the default exit code to raise when --force_exit_code is not defined
 
     try:
         if args.force_error.lower() == "before":
